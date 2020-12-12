7c80fca207c8735c14837fc535eb12f68195b4a5
Diego <diego.qn_rf@hotmail.com>
2020-12-12 13:24:18 -0600

    Fixed

--- src/main/java/com/mx/serviceimpl/NumberServiceImpl.java
+++ src/main/java/com/mx/serviceimpl/NumberServiceImpl.java
@@ -11,65 +11,75 @@ import com.mx.repo.NumberRepo;
 
 @Transactional
 @Service
-public class NumberServiceImpl implements NumberService{
+public class NumberServiceImpl implements NumberService {
 
 	@Autowired
 	private NumberRepo numberRepo;
-	
-	
+
+	Number number = new Number();
+	NumberBean numberBean = new NumberBean();
+
 	@Override
 	public NumberBean analyze(Integer x) {
-		
-		
-		Number number = new Number();
-		NumberBean numberBean = new NumberBean();
-		
-		
-		//Function for factorial
-		int num = x;
-		for(int i=x-1; i>0; i--) {
-			num = num * (x-1);
-			x--;
-		}
-		numberBean.setFactorial(num);
-		number.setFactorial(num);
-		
-		
-		//Function for prime
-		boolean control = true;						
-			int num1 = x-1;
-			for(int i=x-1; i>1; i--) {
-				if(control==true) {
-					if(x%(num1)!=0) {
-						control = false;
-					}
-				}
-				num1--;
-			}
-		
-		if(control==true) {
-			numberBean.setPrimeNumber("yes");
-			number.setPrimeNumber("yes");
-		}
-		else {
-			numberBean.setPrimeNumber("no");
-			number.setPrimeNumber("no");
-		}
-		
-		
-		//Function for even
-		if (x%2==0) {
-			number.setEvenNumber("yes");
-			numberBean.setEvenNumber("yes");
-		}
-		else {
-			number.setEvenNumber("no");
-			numberBean.setEvenNumber("no");
-		}
-		
-		
-		numberRepo.save(number);
+
+		this.numberBean.setEvenNumber(this.isEven(x));
+		this.numberBean.setPrimeNumber(this.isPrime(x));
+		this.numberBean.setFactorial(this.getFactorial(x));
+
+		this.number.setEvenNumber(this.isEven(x));
+		this.number.setPrimeNumber(this.isPrime(x));
+		this.number.setFactorial(this.getFactorial(x));
+
+		numberRepo.save(number);	
 		return numberBean;
 	}
 
+	
+	// Function for even
+	public String isEven(Integer j) {
+		String result;
+		if (j % 2 == 0) {
+			result = "yes";
+		} else {
+			result = "no";
+		}
+		return result;
+	}
+
+	
+	// Function for prime
+	public String isPrime(Integer j) {
+		String resul1;
+		boolean control = true;
+		int num1 = j - 1;
+		int i = 0;
+		for (i = j - 1; i > 1; i--) {
+			if (control == true) {
+				if (j % (num1) == 0) {
+					control = false;
+				}
+			}
+			num1--;
+		}
+
+		if (control == true) {
+			resul1 = "yes";
+		} else {
+			resul1 = "no";
+		}
+		return resul1;
+	}
+
+	
+	// Function for factorial
+	public Integer getFactorial(Integer j) {
+		Integer num = j;
+		for (int i = j - 1; i > 0; i--) {
+			num = num * (j - 1);
+			j--;
+		}
+		return num;
+	}
+
+	
 }
